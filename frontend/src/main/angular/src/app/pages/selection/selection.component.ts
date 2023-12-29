b833da9c91ff878eb4cd113aa0ab680277343a79
Gérald Even <gerald.even@arhs-cube.com>
2023-12-29 08:17:12 +0100

    selection page

--- frontend/src/main/angular/src/app/pages/selection/selection.component.ts
+++ frontend/src/main/angular/src/app/pages/selection/selection.component.ts
@@ -1,17 +1,41 @@
 import { Component } from '@angular/core';
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
+import {MatChipListboxChange, MatChipsModule} from "@angular/material/chips";
+import {FormsModule} from "@angular/forms";
+import {JsonPipe, NgIf} from "@angular/common";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatTooltipModule} from "@angular/material/tooltip";
 
 @Component({
   selector: 'app-selection',
   standalone: true,
   imports: [
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatChipsModule,
+    FormsModule,
+    JsonPipe,
+    MatIconModule,
+    MatButtonModule,
+    NgIf,
+    MatTooltipModule
   ],
   templateUrl: './selection.component.html',
   styleUrl: './selection.component.css'
 })
 export class SelectionComponent {
+  protected data={
+    name:'',
+    role:''
+  };
+  roleSelected($event: MatChipListboxChange) {
+    this.data.role=$event.value;
+    console.log(this.data);
+  }
 
+  nameProvided($event: Event) {
+    console.log($event)
+  }
 }
