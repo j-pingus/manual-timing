5b1d11ac78615cb50af7f9cac3138dd73d50661e
Gérald Even <gerald.even@arhs-cube.com>
2023-12-30 17:52:41 +0100

    setup a socks bridge in the backend

--- backend/src/main/java/lu/even/manual_timing/Events.java
+++ backend/src/main/java/lu/even/manual_timing/Events.java
@@ -2,7 +2,8 @@ package lu.even.manual_timing;
 
 public enum Events {
   POOL_CONFIG("timing.pool.config"),
-  REGISTER("timing.register");
+  REGISTER("timing.register"),
+  MESSAGE("timing.message");
   private final String name;
 
   Events(String name){
