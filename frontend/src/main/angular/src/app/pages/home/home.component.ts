c0f1e0f9d5d260e858525dcb0b7abd95c3267c93
Gérald <gerald.even@gmail.com>
2024-01-07 10:22:10 +0100

    add tooltip on home

--- frontend/src/main/angular/src/app/pages/home/home.component.ts
+++ frontend/src/main/angular/src/app/pages/home/home.component.ts
@@ -4,11 +4,12 @@ import {MatCardModule} from "@angular/material/card";
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
 import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatTooltipModule} from "@angular/material/tooltip";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-    imports: [RouterOutlet, MatCardModule, MatIconModule, MatButtonModule, MatToolbarModule],
+  imports: [RouterOutlet, MatCardModule, MatIconModule, MatButtonModule, MatToolbarModule, MatTooltipModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
